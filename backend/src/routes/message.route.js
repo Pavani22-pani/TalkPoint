2476dec20f4eb78f0a006150d44e702609600a06
Pavani <mekalapavani.pani@gmail.com>
2025-07-09 22:35:07 +0530

    Fix path-to-regexp route param error

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,26 @@
+// import express from "express";
+// import { protectRoute } from "../middleware/auth.middleware.js"
+// import { getMessages, getUsersForSidebar, sentMessages } from "../controllers/message.controller.js"
+
+// const router=express.Router()
+
+// router.get("/user",protectRoute,getUsersForSidebar)
+// router.get("/:id",protectRoute,getMessages)
+// router.post("/send/:id",protectRoute,sentMessages)
+
+
+// export default router
+
+
+
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMessages, getUsersForSidebar, sentMessages } from "../controllers/message.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sentMessages } from "../controllers/message.controller.js";
 
-const router=express.Router()
+const router = express.Router();
 
-router.get("/user",protectRoute,getUsersForSidebar)
-router.get("/:id",protectRoute,getMessages)
-router.post("/send/:id",protectRoute,sentMessages)
+router.get("/user", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sentMessages);
 
-
-export default router
+export default router;
