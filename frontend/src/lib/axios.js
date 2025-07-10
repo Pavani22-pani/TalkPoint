c2f2fac18669c0255114de1bab638d2f742424dc
Pavani <mekalapavani.pani@gmail.com>
2025-07-10 19:19:30 +0530

    changed axios.js file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,22 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   // baseURL: import.meta.env.MODE === "development"
+//   //   ? "http://localhost:5001/api"
+//   //   : "/api",
+//   // withCredentials: true,
+//   baseURL: import.meta.env.MODE === "development"
+//   ? "http://localhost:5001/api"
+//   : "https://talkpoint-api.onrender.com/api"
+
+// });
+
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: import.meta.env.MODE === "development"
-  //   ? "http://localhost:5001/api"
-  //   : "/api",
-  // withCredentials: true,
   baseURL: import.meta.env.MODE === "development"
-  ? "http://localhost:5001/api"
-  : "https://talkpoint-api.onrender.com/api"
-
+    ? "http://localhost:5001/api"
+    : "/api", // Production: frontend and backend served from same origin
+  withCredentials: true,
 });
